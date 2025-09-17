4d98ee5d38e7683a2c4f8c80df135a6ddfe51a60
Nagy Máté <morsakk.mate@gmail.com>
2025-09-17 12:13:45 +0200

    post method

--- app.js
+++ app.js
@@ -30,14 +30,26 @@ app.get("/customers", (req, res) => {
   res.status(200).json(customers);
 });
 
-app.get("/customer/:id", (req, res) => {
+app.get("/customers/:id", (req, res) => {
   const id = req.params.id;
   const customer = customers.find((c) => c.id == id);
   if (!customer) {
-    return res.status(404).json({ message: "Customer not found" });
+    return res.status(404).json({ message: "Customer not found!" });
   }
   res.status(200).json(customer);
 });
+
+app.post("/customers", (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    return res.status(400).json({ message: "Invalis credentials!" });
+  }
+  const id = customers[customers.length - 1]?.id + 1 || 1;
+  const customer = { id, name, email };
+  customers.push(customer);
+  res.status(200).json(customer);
+});
+
 
 app.listen(port, () => {
   console.log(`Server runs on ${port}`);
